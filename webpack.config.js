fdf602fbf5e4e248f993c45139eb81382f25fcfd
Narayan Krishna <nkrishn8@gmail.com>
2020-06-23 16:27:24 +0530

    change in package and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports ={
 
-    entry : './src/main.js',
+    entry : './src/index.js',
     output: {
         path : path.resolve(__dirname,'dist/assets'),
         filename : 'bundle.js'
